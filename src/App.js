fdacead6d8a4db23381d4f337e4f37aa96d28eec
srloser <43763504+srloser@users.noreply.github.com>
2019-08-20 13:05:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './components/router/Routes';
+
+function App() {
+  return (
+      <div className="Container">
+        <Routes />
+      </div>
+  );
+}
+
+export default App;
